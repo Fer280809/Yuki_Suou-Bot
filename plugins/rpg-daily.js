e273df314c132216852aaff173d175317c8f05ed
fernando <fernandoagustinservinsanches@gmail.com>
2025-04-09 14:33:49 -0600

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -14,7 +14,9 @@ var handler = async (m, { conn }) => {
     global.db.data.users[m.sender].exp += exp;
     conn.reply(m.chat, `${emoji} *Recompensa Diaria*
 
-Recursos:
+TE HAS ESFORSADO TODO EL DIA DE HOY OPTEN UNA RECOMPENSA POR ESO
+
+RECOMPENSA:
 ✨ Xp : *+${exp}*
 💎 Diamantes : *+${d}*
 💸 ${moneda} : *+${coin}*`, m);
@@ -33,6 +35,16 @@ export default handler;
 function msToTime(duration) {
     var milliseconds = parseInt((duration % 1000) / 100),
         seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    return hours + ' Horas ' + minutes + ' Minutos';
+}
+,
         minutes = Math.floor((duration / (1000 * 60)) % 60),
         hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
